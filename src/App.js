e6e3bd7ad459a58bcd6e0555b6e9af05ef612cf6
Brittany Lindgren <brittdevelopsit@gmail.com>
2021-01-04 16:16:25 -0800

    Delete boilerplate, add a component.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test />
     </div>
   );
 }
 
 export default App;
-
-// • The following have been added as project dependencies: firebase & react-firebase-hooks
-// • Make a change in the Firestore database and it shows up in the app
-// • Make a change in the app and it shows up in the Firestore database
