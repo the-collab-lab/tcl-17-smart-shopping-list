2d9dfd7837576e64f26fa14b703ef6875bbdc590
DeeDowns <dee_downs@outlook.com>
2021-01-05 18:20:57 -0500

    wrap App comp in Router import Link, Switch, Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Test from './components/Test';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Test />
+      <Navigation />
+
+      <Switch></Switch>
     </div>
   );
 }
