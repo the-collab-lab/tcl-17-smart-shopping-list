2bd98bb9d922576dee4ecf232eb5fc2693ddb6f5
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-01-04 17:02:28 -0800

    Add useState and useEffect hooks to make subscription to the firestore database.

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { fb } from '../lib/firebase';
 
 const Test = () => {
   const db = fb.firestore();
+
+  const [test, setTest] = useState([]);
 
   const handleClick = (event) => {
     event.preventDefault();
@@ -11,12 +13,31 @@ const Test = () => {
     });
   };
 
+  useEffect(() => {
+    const unsubscribe = db.collection('tests').onSnapshot((snapshot) => {
+      const allTests = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTest(allTests);
+    });
+    return () => {
+      console.log('cleanup');
+      unsubscribe();
+    };
+  }, [db]);
+
   return (
     <div>
       <h1>Test</h1>
       <button type="button" onClick={handleClick}>
         Click Me!
       </button>
+      <ul>
+        {test.map((test) => (
+          <li key={test.id}>{test.testMessage}</li>
+        ))}
+      </ul>
     </div>
   );
 };
