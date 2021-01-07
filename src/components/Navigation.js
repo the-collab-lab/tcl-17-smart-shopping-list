af6fd1f356c2706bca78599a3b82a6fecf3a12a1
Viet <vietnamtran@gmail.com>
2021-01-06 19:10:43 -0600

    Use active-link as class name, change background color for accessibility

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
   return (
     <nav>
-      <NavLink to="/list" activeClassName="list-link">
+      <NavLink to="/list" activeClassName="active-link">
         <div className="list-link-text">List</div>
       </NavLink>
-      <NavLink to="/add-item" activeClassName="add-link">
+      <NavLink to="/add-item" activeClassName="active-link">
         Add Item
       </NavLink>
     </nav>
