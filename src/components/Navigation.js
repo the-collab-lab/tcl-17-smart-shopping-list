6c478278e4ec00e3f3d5060c7a040f07962b2a05
Viet <vietnamtran@gmail.com>
2021-01-06 18:10:18 -0600

    Use activeClassName over useState

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
-  const [listFontWeight, setListFontWeight] = useState('normal');
-  const [addFontWeight, setAddFontWeight] = useState('normal');
-
-  const toggleBold = (event) => {
-    if (event.target.className === 'list-link') {
-      setListFontWeight('bold');
-      setAddFontWeight('normal');
-    }
-    if (event.target.className === 'add-link') {
-      setAddFontWeight('bold');
-      setListFontWeight('normal');
-    }
-  };
   return (
     <nav>
-      <Link
-        to="/list"
-        className="list-link"
-        onClick={toggleBold}
-        style={{ fontWeight: listFontWeight }}
-      >
-        List
-      </Link>
-      <Link
-        to="/add-item"
-        className="add-link"
-        onClick={toggleBold}
-        style={{ fontWeight: addFontWeight }}
-      >
-        Add an Item
-      </Link>
+      <NavLink to="/list" activeClassName="list-link">
+        <div className="list-link-text">List</div>
+      </NavLink>
+      <NavLink to="/add-item" activeClassName="add-link">
+        Add Item
+      </NavLink>
     </nav>
   );
 };
