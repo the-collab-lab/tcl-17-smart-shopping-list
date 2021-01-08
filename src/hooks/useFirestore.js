225d80ffe655b9bb60bdc013bd772f9000bc1ff4
Brittany Lindgren <brittdevelopsit@gmail.com>
2021-01-07 17:01:14 -0800

    Create custom hook to get data from firestore.

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -5,22 +5,18 @@ const useFirestore = (collection) => {
   const [docs, setDocs] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = db.collection(collection).onSnapshot(
-      (snapshot) => {
-        let documents = [];
-        snapshot.forEach((doc) => {
-          documents.push({ ...doc.data(), id: doc.id });
-        });
-        setDocs(documents);
-        return () => {
-          unsubscribe();
-        };
-      },
-      [collection],
-    );
-
-    return { docs };
-  });
+    const unsubscribe = db.collection(collection).onSnapshot((snapshot) => {
+      let documents = [];
+      snapshot.forEach((doc) => {
+        documents.push({ ...doc.data(), id: doc.id });
+      });
+      setDocs(documents);
+      return () => {
+        unsubscribe();
+      };
+    });
+  }, [collection]);
+  return { docs };
 };
 
 export default useFirestore;
