d39ea28944ef3d0a2738ce0ee18589f5a73bc3c8
Brittany Lindgren <brittdevelopsit@gmail.com>
2021-01-11 14:17:27 -0800

    Add comment with issue AC to project.

--- src/App.js
+++ src/App.js
@@ -31,3 +31,17 @@ function App() {
 }
 
 export default App;
+
+// A shopping list consists of a set of items associated with a user’s token. Creating a new list consists of the following sequence:
+
+// Generate a new, unique token
+// Save the token to localStorage
+// Show the user the list view
+// The following script should be used to generate a suitable token: src/lib/tokens.js
+
+// AC:
+
+// For users who do not already have a token/list, a button or link exists on the home screen that allows them to create a new list
+// Clicking the button/link generates a new token and saves it to localStorage
+// Once the token has been created and saved, the user is redirected to the "list" view
+// For users who do have a token already saved in localStorage, they should be shown/redirected to the "list" view
