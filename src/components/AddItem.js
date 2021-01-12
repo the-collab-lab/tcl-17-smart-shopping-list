141df18af814096ab033fb67b803dc34bdade5b7
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-01-11 18:58:39 -0800

    add comments to describe list item object

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -9,3 +9,11 @@ const AddItem = () => {
 };
 
 export default AddItem;
+
+//A shopping list item consists of the following data points:
+// Name of item
+// How soon are you likely to buy it again?
+//  Soon (in the next 7 days)
+//  Kind of soon (in the next 14 days)
+//  Not soon (in the next 30 days)
+// Last purchased date
