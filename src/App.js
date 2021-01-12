7720a907417257c9188dbea36c5593d4cf64cd22
Viet <vietnamtran@gmail.com>
2021-01-12 12:57:28 -0600

    Check for token in localStorage, redirect to List if exists

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Welcome from './components/Welcome';
 import getToken from './lib/tokens';
 
 function App() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState(localStorage.getItem('userToken') || '');
 
   const handleClick = () => {
     const token = getToken();
@@ -38,17 +38,3 @@ function App() {
 }
 
 export default App;
-
-// A shopping list consists of a set of items associated with a user’s token. Creating a new list consists of the following sequence:
-
-// Generate a new, unique token
-// Save the token to localStorage
-// Show the user the list view
-// The following script should be used to generate a suitable token: src/lib/tokens.js
-
-// AC:
-
-// For users who do not already have a token/list, a button or link exists on the home screen that allows them to create a new list X
-// Clicking the button/link generates a new token and saves it to localStorage X
-// Once the token has been created and saved, the user is redirected to the "list" view X
-// For users who do have a token already saved in localStorage, they should be shown/redirected to the "list" view
