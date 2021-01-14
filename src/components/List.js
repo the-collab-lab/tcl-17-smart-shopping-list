e3d5ef2517fd74172a49352e8a51d64373b7f47c
DeeDowns <dee_downs@outlook.com>
2021-01-14 16:08:05 -0500

    use firestore hook to fetch, map through, and display data

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import useFirestore from '../hooks/useFirestore';
 
 const List = () => {
+  const { docs } = useFirestore('user token');
+  console.log(docs);
   return (
     <div>
       <h1>List</h1>
+      <ul>{docs && docs.map((doc) => <li key={doc.id}>{doc.itemName}</li>)}</ul>
     </div>
   );
 };
