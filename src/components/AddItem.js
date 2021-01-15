1442e2552a1b5cd4abc51eb735ac9a6a75707a90
DeeDowns <dee_downs@outlook.com>
2021-01-15 17:17:53 -0500

    change timeFrame value to number; add required prop to item name input, remove event.preventDefault from handleFormChange

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -8,31 +8,27 @@ const initialFormState = {
   lastPurchased: null,
 };
 
-const AddItem = () => {
-  const token = 'user token';
+const AddItem = ({ token }) => {
   const history = useHistory();
   const [formData, setFormData] = useState(initialFormState);
-  const [error, setError] = useState('');
-  console.log(error);
 
   const handleFormChange = (event) => {
-    event.preventDefault();
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
+    event.target.type === 'radio'
+      ? setFormData({
+          ...formData,
+          [event.target.name]: parseInt(event.target.value),
+        })
+      : setFormData({
+          ...formData,
+          [event.target.name]: event.target.value,
+        });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!formData.itemName) {
-      setError(`${event.target.name} field cannot be empty`);
-    } else {
-      db.collection(token).add(formData);
-      history.push('/list');
-      setFormData(initialFormState);
-      setError('');
-    }
+    db.collection(token).add(formData);
+    setFormData(initialFormState);
+    history.push('/list');
   };
 
   return (
@@ -46,10 +42,10 @@ const AddItem = () => {
             type="text"
             placeholder="add your item here"
             name="itemName"
+            required={true}
             value={formData.itemName}
             onChange={handleFormChange}
           />
-          {error && <span>{error}</span>}
         </label>
 
         <fieldset className="fieldset">
@@ -85,18 +81,10 @@ const AddItem = () => {
           <label htmlFor="not-soon">Not Soon</label>
           <br />
         </fieldset>
-        <button>Add Item</button>
+        <button type="submit">Add Item</button>
       </form>
     </div>
   );
 };
 
 export default AddItem;
-
-//A shopping list item consists of the following data points:
-// Name of item
-// How soon are you likely to buy it again?
-//  Soon (in the next 7 days)
-//  Kind of soon (in the next 14 days)
-//  Not soon (in the next 30 days)
-// Last purchased date
