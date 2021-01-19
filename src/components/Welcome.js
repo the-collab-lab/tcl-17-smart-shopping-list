3ac4e2e885db5ac3145bfed4e2c508ee007f7b9e
Brittany Lindgren <brittdevelopsit@gmail.com>
2021-01-19 13:10:17 -0800

    Add AC as comments in Welcome.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -15,3 +15,8 @@ Welcome.propTypes = {
 };
 
 export default Welcome;
+
+// When the user does not already have a token in localStorage, on the onboarding/home screen, a simple form is displayed that allows the user to enter a token
+// Entering the token and hitting submit saves the token to localStorage, effectively giving them joint control of the list
+// On submit, show an error if the token does not exist
+// If they get an error message, allow them to try again or create a new list
