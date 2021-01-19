72fef5e33f6e85bb87d9bc2150b86f989f37af99
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-01-19 15:13:16 -0800

    add component for duplicate error message

---
+++ src/components/Duplicate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Duplicate = () => {
+  return (
+    <div>
+      <p style={{ color: 'red' }}>That item is already in your list</p>
+    </div>
+  );
+};
+
+export default Duplicate;
