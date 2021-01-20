d43ecda80edbfaf28ffa8762e45e748076f49295
DeeDowns <dee_downs@outlook.com>
2021-01-20 14:33:38 -0500

    create query to check if input token is valid

--- src/components/TokenForm.js
+++ src/components/TokenForm.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { db } from '../lib/firebase';
 
 const TokenForm = () => {
   const [tokenInput, setTokenInput] = useState('');
+  const history = useHistory();
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const collectionRef = db.collection('userTokens');
+    const tokenWeAreCheckingFor = await collectionRef
+      .where('tokenName', '==', tokenInput)
+      .get();
+    if (tokenWeAreCheckingFor.empty) {
+      console.log('no token');
+      // error message
+    } else {
+      console.log('you found it ', tokenWeAreCheckingFor);
+      localStorage.setItem('userToken', tokenInput);
+      // history.push('/list')
+    }
+  };
+
   return (
     <React.Fragment>
       <div>
         <h3>Share Token</h3>
-        <form onSubmit={console.log(tokenInput)}>
+        <form onSubmit={handleFormSubmit}>
           <label htmlFor="token">
             <input
               id="token"
