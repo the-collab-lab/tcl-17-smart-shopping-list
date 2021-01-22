a795a3c0035f38ee181e48faba65bb6f4af7323a
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-01-21 16:40:16 -0800

    Add files from BL-DD-join-existing-list

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TokenForm from './TokenForm';
 
-const Welcome = ({ onClick }) => {
+const Welcome = ({ onClick, setToken }) => {
   return (
     <div>
       <h1>Welcome</h1>
       <button onClick={onClick}>Create a New List</button>
+      <TokenForm setToken={setToken} />
     </div>
   );
 };
@@ -15,3 +17,5 @@ Welcome.propTypes = {
 };
 
 export default Welcome;
+
+//change [hidden, setHidden] to [error, setError] ? check with Rachel and Viet
