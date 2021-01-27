bd26f08d249e2e28debf8dbae03177433750397a
DeeDowns <dee_downs@outlook.com>
2021-01-26 21:20:29 -0500

    create checkbox for items, work on updating date

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore';
+import { db } from '../lib/firebase';
 
 const List = ({ token }) => {
   const { docs } = useFirestore(token);
+  const handleCheckbox = async (event) => {
+    const currentDate = new Date();
+    const nextDay = new Date();
+    // localStorage.nextDay = nextDay.setDate(nextDay.getDate() + 1)
+    console.log(event.target);
+    const queryCollection = await db
+      .collection(token)
+      .doc(event.target.id)
+      .get();
+    // const snapshot = await queryCollection.get()
+    queryCollection.data();
+    console.log(queryCollection.data());
+  };
   return (
     <div>
       <h1>List</h1>
-      <ul>{docs && docs.map((doc) => <li key={doc.id}>{doc.itemName}</li>)}</ul>
+      <ul style={{ listStyleType: 'none' }}>
+        {docs &&
+          docs.map((doc) => (
+            <li key={doc.id}>
+              <input
+                type="checkbox"
+                name={doc.itemName}
+                id={doc.id}
+                onClick={handleCheckbox}
+              />
+              {doc.itemName}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
