4a91d1d7f4cdddff6c1fb469eac99fedefa98f0e
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-01-29 11:01:04 -0800

    Change button to link and remove history

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import useFirestore from '../hooks/useFirestore';
 
 const List = ({ token }) => {
   const { docs } = useFirestore(token);
-  const history = useHistory();
 
   return (
     <div>
       <h1>List</h1>
       {docs.length === 0 && (
-        <button onClick={() => history.push('/add-item')}>Add an Item</button>
+        <div>
+          <p>Your shopping list is currently empty.</p>
+          <a href="/add-item">Add an Item</a>
+        </div>
       )}
       <ul>{docs && docs.map((doc) => <li key={doc.id}>{doc.itemName}</li>)}</ul>
     </div>
@@ -18,7 +19,3 @@ const List = ({ token }) => {
 };
 
 export default List;
-
-//Issue 7 AC:
-// The list view, when there are no items to display, should show a prompt (e.g., a button) for the user to add their first item
-// When the user clicks on the add item Button, they should be routed to the Add Item page.
