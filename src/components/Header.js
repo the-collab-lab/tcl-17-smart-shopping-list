e9d49e239c1d14efeda114a2cb8dc1555c91f6e3
DeeDowns <dee_downs@outlook.com>
2021-02-25 18:34:35 -0500

    create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <p>logo</p>
+      <h1>Smart Shopper</h1>
+    </header>
+  );
+};
+
+export default Header;
+
+// There is a header element to separate the title and logo from main part of the app
+// There is a border to separate navigation elements from main part of the app
+// The header background color and border color should be the same
+// The List page and Add Item page will have the same styles applied
