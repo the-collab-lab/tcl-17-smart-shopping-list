8da97fb5e504060d5af271a16f8aa4ad28101241
Brittany Lindgren <brittdevelopsit@gmail.com>
2021-02-24 16:10:43 -0800

    Add icons and svg to navigation.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import homeIcon from './../img/groceries.svg';
 
 const Navigation = () => {
   return (
     <nav>
+      <NavLink to="/" activeClassName="active-link">
+        <div className="textWithIcon">
+          <span>Home</span>{' '}
+          <img className="homeIcon" src={homeIcon} alt="logo and home link" />
+        </div>
+      </NavLink>
       <NavLink to="/list" activeClassName="active-link">
-        <div className="list-link-text">List</div>
+        <div className="textWithIcon">
+          <span>Shopping List</span> <i class="fas fa-list-ul"></i>
+        </div>
       </NavLink>
       <NavLink to="/add-item" activeClassName="active-link">
         <span>
@@ -17,3 +26,5 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+// MUST ADD CREDIT SOMEWHERE <div>Icons made by <a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart">smalllikeart</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
