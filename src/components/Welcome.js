680c5d9fc095a0c5047693821a0732d06ae5e53c
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-02-25 23:11:43 -0800

    Update headers and button text to match wireframe

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TokenForm from './TokenForm';
+import './../styles/Welcome.css';
 
 const Welcome = ({ onClick, setToken }) => {
   return (
-    <div>
-      <h1>Welcome</h1>
-      <button onClick={onClick}>Create a New List</button>
-      <TokenForm setToken={setToken} />
+    <div className="background">
+      <div className="welcome-container">
+        <div className="create-list-container">
+          {/* <h1>Welcome</h1> */}
+          <h3 className="create-list-header">
+            Get Started! <br />
+            Create New List
+          </h3>
+          <button className="welcome-button" onClick={onClick}>
+            New List
+          </button>
+        </div>
+        <TokenForm setToken={setToken} />
+      </div>
     </div>
   );
 };
@@ -19,3 +30,7 @@ Welcome.propTypes = {
 export default Welcome;
 
 //change [hidden, setHidden] to [error, setError] ? check with Rachel and Viet
+
+// <img className="groceryAisle" src={groceryAisle} alt="grocery aisle"></img>
+
+// style={{ backgroundImage: `url(${groceryAisle})
