925b87ca8976cdcfff122e4608d227ad3cb0927d
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-02-25 23:10:54 -0800

    Add formatting to token form

--- src/components/TokenForm.js
+++ src/components/TokenForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { db } from '../lib/firebase';
 import Error from './Error';
+import './../styles/TokenForm.css';
 
 const TokenForm = ({ setToken }) => {
   const [tokenInput, setTokenInput] = useState('');
@@ -23,24 +24,28 @@ const TokenForm = ({ setToken }) => {
 
   return (
     <React.Fragment>
-      <div>
-        <h3>Share Token</h3>
+      <div className="join-list-container">
+        <h3 className="join-list-header">Join Existing List</h3>
         <form onSubmit={handleFormSubmit}>
-          <label htmlFor="token">
-            <input
-              id="token"
-              type="text"
-              placeholder="three word token"
-              name="token"
-              value={tokenInput}
-              onChange={(event) => {
-                setTokenInput(event.target.value);
-                setError(false);
-              }}
-              required
-            />
-          </label>
-          <button type="submit">Join an existing list</button>
+          <div className="join-form">
+            <label htmlFor="token">
+              <input
+                id="token"
+                type="text"
+                placeholder="three word token"
+                name="token"
+                value={tokenInput}
+                onChange={(event) => {
+                  setTokenInput(event.target.value);
+                  setError(false);
+                }}
+                required
+              />
+            </label>
+            <button className="welcome-button" type="submit">
+              Join
+            </button>
+          </div>
         </form>
         {error && (
           <Error errorMessage="Hmmm... we couldn't find that list. Please try again or create a new list." />
