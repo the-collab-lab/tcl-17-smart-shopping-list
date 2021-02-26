9752ba8904c82f3ac9cad101280c145b6ed59221
DeeDowns <dee_downs@outlook.com>
2021-02-25 20:53:08 -0500

    add logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/Header.css';
+import homeIcon from './../img/groceries.svg';
 
 const Header = () => {
   return (
     <header>
-      <p>logo</p>
+      <img
+        src={homeIcon}
+        className="homeIcon"
+        alt="colorful grocery bag logo and home link"
+      />
       <h1>Smart Shopper</h1>
     </header>
   );
