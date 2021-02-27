cb1d8473769231c90635a37b8019333b8b06eb40
Brittany Lindgren <brittdevelopsit@gmail.com>
2021-02-27 11:29:32 -0800

    Update root color to blue from project palette.

--- src/components/List.js
+++ src/components/List.js
@@ -14,7 +14,7 @@ import './../styles/AddItem.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    color: theme.palette.primary.main,
+    color: theme.palette.ourblue.main,
   },
 }));
 
