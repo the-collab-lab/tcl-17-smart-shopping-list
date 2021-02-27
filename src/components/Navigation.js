a911a58d4ea26e89ba85b78e349957122171086f
DeeDowns <dee_downs@outlook.com>
2021-02-27 18:46:31 -0500

    initial commit draft pr

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -28,5 +28,8 @@ const Navigation = ({ confirmDeleteUserToken }) => {
 };
 
 export default Navigation;
+// Definition of done :
+
+// Material UI is implemented for the bottom navigation
 
 // MUST ADD CREDIT SOMEWHERE <div>Icons made by <a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart">smalllikeart</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
