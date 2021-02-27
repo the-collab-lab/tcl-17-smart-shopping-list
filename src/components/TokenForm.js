d7ba57094c3e94689707e10a8302be1872200b75
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-02-26 16:30:26 -0800

    Switch to use MaterialUI box, button, and textfield components for uniformity

--- src/components/TokenForm.js
+++ src/components/TokenForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { db } from '../lib/firebase';
 import Error from './Error';
 import './../styles/TokenForm.css';
+import { Box, Button, TextField } from '@material-ui/core';
 
 const TokenForm = ({ setToken }) => {
   const [tokenInput, setTokenInput] = useState('');
@@ -24,33 +25,44 @@ const TokenForm = ({ setToken }) => {
 
   return (
     <React.Fragment>
-      <div className="join-list-container">
-        <h3 className="join-list-header">Join Existing List</h3>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="space-around"
+        className="join-list-container"
+      >
+        <h3 className="join-list-header">Join an Existing List</h3>
         <form onSubmit={handleFormSubmit}>
-          <div className="join-form">
-            <label htmlFor="token">
-              <input
-                id="token"
-                type="text"
-                placeholder="three word token"
-                name="token"
-                value={tokenInput}
-                onChange={(event) => {
-                  setTokenInput(event.target.value);
-                  setError(false);
-                }}
-                required
-              />
-            </label>
-            <button className="welcome-button" type="submit">
-              Join
-            </button>
-          </div>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-between"
+            className="join-form"
+          >
+            <TextField
+              variant="outlined"
+              id="token"
+              type="text"
+              label="three word token"
+              name="token"
+              value={tokenInput}
+              onChange={(event) => {
+                setTokenInput(event.target.value);
+                setError(false);
+              }}
+              required
+            />
+
+            <Button variant="contained" id="join-button" type="submit">
+              Join List
+            </Button>
+          </Box>
         </form>
         {error && (
           <Error errorMessage="Hmmm... we couldn't find that list. Please try again or create a new list." />
         )}
-      </div>
+      </Box>
     </React.Fragment>
   );
 };
